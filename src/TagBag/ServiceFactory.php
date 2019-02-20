e00a30eba0e319eb211b50469ac53c8e9b281f3e
Joachim Løvgaard <joachim@loevgaard.dk>
2019-02-20 09:03:45 +0100

    added comment to explain service factory

--- src/TagBag/ServiceFactory.php
+++ src/TagBag/ServiceFactory.php
@@ -6,6 +6,11 @@ namespace Setono\TagBagBundle\TagBag;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+/**
+ * This class will return the tag bag. The reason why we have to do it this is because
+ * Symfony first starts the session when `getBag()` is called on the session service.
+ * Using a service factory we are sure that people can autowire the TagBagInterface.
+ */
 class ServiceFactory
 {
     public static function create(SessionInterface $session): TagBagInterface
